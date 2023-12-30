b50dfcc824dd91dae2137d645dc02bccf728e15d
SarbasisMB <sarbasismbanerjee@gmail.com>
2023-12-30 11:54:54 +0530

    Third Code

---
+++ Day1/03_Arithmetic_Operators.py
@@ -0,0 +1,14 @@
+'''
+Task:
+    Provided two integers, 'a' and 'b'.
+    There will be three outputs:
+    1. First line contains sum of two numbers.
+    2. Second line contains difference of two numbers (first - second)
+    3. Third line contains product of two numbers.
+'''
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(f"{a+b}")
+    print(f"{a-b}")
+    print(f"{a*b}")
