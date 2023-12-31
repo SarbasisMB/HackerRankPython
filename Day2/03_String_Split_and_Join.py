670dd24af69fd014913c9bda1522686a90245e6a
SarbasisMB <sarbasismbanerjee@gmail.com>
2023-12-31 23:39:33 +0530

    Day 2 Program 3

---
+++ Day2/03_String_Split_and_Join.py
@@ -0,0 +1,14 @@
+'''
+    This is a program to split the given string.
+    Then replace the space with '-'
+'''
+
+
+def split_and_join(line):
+    # write your code here
+    pass
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
